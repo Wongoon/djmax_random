21d4aadaee39654b9fb1e83fa9a9256852c479a9
Wongoon <rmsgh64@gmail.com>
2022-09-22 12:59:36 +0900

    code update

--- javascript/index.js
+++ javascript/index.js
@@ -3,7 +3,17 @@ const sidebar = body.querySelector('nav');
 const toggle = body.querySelector(".toggle");
 const searchBtn = body.querySelector(".search-box");
 const searchBox = document.getElementById('search');
+const randomlist = body.querySelectorAll('#tab-02 .list li');
+var requestURL = 'json/list.json';
+var request = new XMLHttpRequest();
+var songlist;
 
+request.open('GET', requestURL);
+request.responseType = 'json';
+request.send();
+request.onload = function(){
+    songlist = request.response;
+}
 
 toggle.addEventListener("click", () => {
     sidebar.classList.toggle("close");
@@ -16,3 +26,25 @@ searchBtn.addEventListener("click", () => {
 searchBox.addEventListener('click', function(){
     alert('아직 준비 중인 기능입니다.');
 });
+
+for(let el of randomlist){
+    el.addEventListener('click', e=>{
+        e.currentTarget.classList.toggle('on');
+        if(e.currentTarget.getAttribute('class') == 'on'){
+            addsong(songlist);
+        }
+        else{
+            removesong(songlist);
+        }
+    })
+}
+
+// e.currentTarget.getAttribute('class') 클래스 가져오기
+
+function addsong(song){
+    console.log(song);
+}
+
+function removesong(song){
+    console.log(song);
+}
--- javascript/tabmenu.js
+++ javascript/tabmenu.js
@@ -8,4 +8,15 @@ $(document).ready(function(){
         $(this).addClass('current');
         $('#' + tab).addClass('active');
     });
+
+    $('#tab-01 ul.list li').click(function(){
+        var tab = $(this).attr('data-tab');
+        var split = tab.split('-');
+
+        $('ul.menu-links li').removeClass('current');
+        $('.tab-content').removeClass('active');
+
+        $('#'+split[0]+split[1]).addClass('current');
+        $('#' + tab).addClass('active');
+    });
 });
