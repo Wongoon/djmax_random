7f165ea66721b4d087052cc185e9ea689deed733
Wongoon <rmsgh64@gmail.com>
2022-09-23 14:52:57 +0900

    code update

--- javascript/index.js
+++ javascript/index.js
@@ -4,6 +4,7 @@ const toggle = body.querySelector(".toggle");
 const searchBtn = body.querySelector(".search-box");
 const searchBox = document.getElementById('search');
 const randomlist = body.querySelectorAll('#tab-02 .list li');
+const ul = document.getElementById('songlist');
 var requestURL = 'json/list.json';
 var request = new XMLHttpRequest();
 var songlist;
@@ -31,20 +32,30 @@ for(let el of randomlist){
     el.addEventListener('click', e=>{
         e.currentTarget.classList.toggle('on');
         if(e.currentTarget.getAttribute('class') == 'on'){
-            addsong(songlist);
+            addsong(songlist, e.currentTarget.getAttribute('id'));
         }
         else{
-            removesong(songlist);
+            removesong(songlist, e.currentTarget.getAttribute('id'));
         }
-    })
+    });
 }
-
 // e.currentTarget.getAttribute('class') 클래스 가져오기
 
-function addsong(song){
-    console.log(song);
+function addsong(song, id){
+    Object.keys(song['songs'][id]).forEach(function(key){
+        const li = document.createElement('li');
+        li.setAttribute('class', id);
+        const text = document.createTextNode(song['songs'][id][key]['title']);
+        li.appendChild(text);
+        ul.appendChild(li);
+    });
 }
 
-function removesong(song){
-    console.log(song);
+function removesong(song, id){
+    Object.keys(song['songs'][id]).forEach(function(key){
+        const li = document.querySelectorAll('#songlist li');
+        Object.keys(li).forEach(function(){
+            this.remove();
+        });
+    });
 }
