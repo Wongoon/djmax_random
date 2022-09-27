d023961758f4c57470878c0a0be4b2313610cd43
Wongoon <rmsgh64@gmail.com>
2022-09-28 08:30:25 +0900

    code update

--- javascript/tabmenu.js
+++ javascript/tabmenu.js
@@ -19,4 +19,14 @@ $(document).ready(function(){
         $('#'+split[0]+split[1]).addClass('current');
         $('#' + tab).addClass('active');
     });
+
+    $('#tab-02 ul#tab-content-list li').click(function(){
+        var tab = $(this).attr('data-set');
+
+        $('ul#tab-content-list li').removeClass('on');
+        $('.set-content').removeClass('on');
+
+        $('#' + tab).addClass('on');
+        $(this).addClass('on');
+    });
 });
